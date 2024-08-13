548f6013c7a13dbb625665f7bdcd3f31f728f077
Shruti Kalekar <shrutikalekar090@gmail.com>
2024-08-13 18:53:18 +0530

    landing page done

---
+++ data/index.ts
@@ -0,0 +1,5 @@
+export const gridItems = [
+    { title:"title 1", description:"description1", id:1 },
+    { title:"title 2", description:"description2", id:2 },
+    { title:"title 3", description:"description3", id:3 },
+    { title:"title 4", description:"description4", id:4 }]
